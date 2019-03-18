745b03ecc21b4b2e0d04bca4aa838271b34c2940
rrsmall <46974871+rrsmall@users.noreply.github.com>
2019-03-18 07:34:20 -0400

    registration form to sign up

--- forms.py
+++ forms.py
@@ -4,3 +4,11 @@ class SearchForm(Form):
     choices = [('Name', 'Name'), ('ID', 'ID'), ('City', 'City'), ('Stars', 'Stars'), ('Categories', 'Categories')]
     select = SelectField('Search for restaurants:', choices=choices)
     search = StringField('')
+
+class RegistrationForm(Form):
+    username = TextField('Username', [validators.Length(min=4, max=20)])
+    email = TextField('Email Address', [validators.Length(min=6, max=50)])
+    password = PasswordField('New Password', [validators.Required(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
