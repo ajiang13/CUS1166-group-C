8a000a190b55883fb404d67032d0a36b006211be
Jajhoey <john.hoey15@my.stjohns.edu>
2019-03-18 21:16:45 -0400

    Created helper functions for sorting

--- db.py
+++ db.py
@@ -14,3 +14,43 @@ def search_business_name(search):
 def search_business_count(search):
     result_count = db.business.find({'$text': {'$search': search}}).count()
     return result_count
+
+#Funtions for sorting results
+def sort_by_name(results,reverse):
+    if (reverse == True):
+        return results.sort('name', -1)
+    else:
+        return results.sort('name')
+
+def sort_by_city(results,reverse):
+    if (reverse == True):
+        return results.sort('city', -1)
+    else:
+        return results.sort('city')
+
+def sort_by_state(results,reverse):
+    if (reverse == True):
+        return results.sort('state', -1)
+    else:
+        return results.sort('state')
+
+#We want businesses with more stars to
+#appear first, so the sort is flipped
+def sort_by_stars(results,reverse):
+    if (reverse == True):
+        return results.sort('stars')
+    else:
+        return results.sort('stars', -1)
+
+#This sort is also flipped compared to the others
+def sort_by_review_count(results,reverse):
+    if (reverse == True):
+        return results.sort('review_count')
+    else:
+        return results.sort('review_count', -1)
+
+def sort_by_category(results,reverse):
+    if (reverse == True):
+        return results.sort('categories', -1)
+    else:
+        return results.sort('catergories')
--- forms.py
+++ forms.py
@@ -5,10 +5,14 @@ class SearchForm(Form):
     select = SelectField('Search for restaurants:', choices=choices)
     search = StringField('')
 
-class RegistrationForm(Form):
-    username = TextField('Username', [validators.Length(min=4, max=20)])
-    email = TextField('Email Address', [validators.Length(min=6, max=50)])
-    password = PasswordField('New Password', [validators.Required(),
-        validators.EqualTo('confirm', message='Passwords must match')
-    ])
-    confirm = PasswordField('Repeat Password')
+#class RegistrationForm(Form):
+    #username = TextField('Username', [validators.Length(min=4, max=20)])
+    #email = TextField('Email Address', [validators.Length(min=6, max=50)])
+    #password = PasswordField('New Password', [validators.Required(),
+    #    validators.EqualTo('confirm', message='Passwords must match')
+    #])
+    #confirm = PasswordField('Repeat Password')
+
+class FilterForm(Form):
+    choices = [('Name', 'Name'), ('ID', 'ID'), ('City', 'City'), ('Stars', 'Stars'), ('Categories', 'Categories')]
+    select = SelectField('Filter by: ', choices = choices)
