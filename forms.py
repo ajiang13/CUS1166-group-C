2a91443b85fadadb3cb9eaa4f38b12f01c65962f
Matt Freedman <matthew.freedman15@stjohns.edu>
2019-03-20 14:34:22 -0400

    Changes to display info

--- forms.py
+++
@@ -1,6 +0,0 @@
-from wtforms import Form, StringField, SelectField
-
-class SearchForm(Form):
-    choices = [('Name', 'Name'), ('ID', 'ID'), ('City', 'City'), ('Stars', 'Stars'), ('Categories', 'Categories')]
-    select = SelectField('Search for restaurants:', choices=choices)
-    search = StringField('')
