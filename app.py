3c2452239be877b43a9aa9ef7d81afd9c26d5a4a
Alan Jiang <alan.jiang13@stjohns.edu>
2019-03-21 12:34:39 -0400

    clean up

--- app.py
+++ app.py
@@ -87,15 +87,5 @@ def login():
     else:
         return render_template('login.html', error=error)
 
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please Try Again.'
-        else:
-            return redirect('/')
-    return render_template('login.html', error=error)
-    
 if __name__ == "__main__":
     app.run(debug=True, host='127.0.0.1', port=5110)
