c6778dd7f534dd3b1eb993b02635e85cb161b55d
Alan Jiang <alan.jiang13@stjohns.edu>
2019-03-21 12:29:54 -0400

    clean up

--- table.py
+++
@@ -1,9 +0,0 @@
-from flask_table import Table, Col
-
-class Restaurant Info(Table):
-    id = Col('Id', show=False)
-    Name = Col('Name')
-    City = Col('City')
-    State = Col('State')
-    Stars = Col('Stars')
-    Postal_Code = Col('Postal_Code')
