a19559718c103715d5516ed4cef8df61baca89f5
rrsmall <46974871+rrsmall@users.noreply.github.com>
2019-05-01 11:23:07 -0400

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,38 +1,67 @@
-from wtforms import StringField, SelectField, TextField, FloatField, PasswordField, validators
-from flask_wtf import Form
+from wtforms import StringField, SelectField, TextField, FloatField, \
+    PasswordField, validators
+from flask_wtf import FlaskForm
 
-class SearchForm(Form):
-    choices = [('Name', 'Name'), ('City', 'City'), ('State', 'State Code'), ('Categories', 'Categories')]
+
+class SearchForm(FlaskForm):
+    choices = [('Name', 'Name'),
+               ('City', 'City'),
+               ('State', 'State Code'),
+               ('Categories', 'Categories')]
     select = SelectField('Search for restaurants:', choices=choices)
     search = StringField('')
     stars = FloatField('Search by rating e.g. 4.5 (and above):')
 
-class AdvancedSearchForm(Form):
+
+class AdvancedSearchForm(FlaskForm):
     name = StringField('Name')
     city = StringField('City')
     state = StringField('State Code')
     categories = StringField('Categories')
     stars = FloatField('Rating')
-    
-class LoginForm(FlaskForm):
-    username = StringField('username',[validators.DataRequired(),validators.Length(min=1, max=100)])
-    password = PasswordField('password',[validators.DataRequired(),validators.Length(min=1, max=100)])
-    
-class RegistrationForm(Form):
+
+
+class RegistrationForm(FlaskForm):
     name = [StringField('Name', [validators.Length(min=1, max=100)])]
     username = StringField('Username', [validators.Length(min=4, max=20)])
     email = StringField('Email Address', [validators.Length(min=6, max=50)])
-    password = PasswordField('New Password', [validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords must match')
-    ])
+    password = PasswordField(
+              'New Password',
+              [validators.DataRequired(),
+               validators.EqualTo(
+               'confirm',
+               message='Passwords must match')])
     confirm = PasswordField('Confirm Password')
 
-class FilterForm(Form):
-    choices = [('Name', 'Name'), ('City', 'City'), ('State', 'State'), ('Stars', 'Stars'), ('Reviews', 'Reviews')]
-    select = SelectField('Sort by: ', choices = choices)
 
-class RestaurantForm(Form):
-    is_open = [('Open', 'Open'), ('Closed','Closed')]
+class LoginForm(FlaskForm):
+    username = StringField(
+              'username',
+              [validators.DataRequired(),
+               validators.Length(min=1, max=100)])
+    password = PasswordField(
+              'password',
+              [validators.DataRequired(),
+               validators.Length(min=1, max=100)])
+
+
+class FilterForm(FlaskForm):
+    choices = [('Name', 'Name'),
+               ('City', 'City'),
+               ('State', 'State'),
+               ('Stars', 'Stars'),
+               ('Reviews', 'Reviews')]
+    select = SelectField('Sort by: ', choices=choices)
+
+
+class DisplayForm(FlaskForm):
+    hours = StringField('Hours')
+    latitude = StringField('Latitude')
+    longitude = StringField('Longitude')
+
+
+class RestaurantForm(FlaskForm):
+    is_open = [('Open', 'Open'), ('Closed', 'Closed')]
     name = StringField('Name')
     city = StringField('City')
     state = StringField('State')
@@ -42,3 +71,13 @@ class RestaurantForm(Form):
     hours = StringField('hours')
     categories = StringField('categories')
     is_open = SelectField('Availability', choices=is_open)
+
+
+class MailForm(FlaskForm):
+    recipients = StringField(
+                'Recipient(s)',
+                [validators.InputRequired()])
+    body = StringField('Body')
+
+class RandomForm(FlaskForm):
+    random="Can't decide? Generate a random restaurant!"
